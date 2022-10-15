69e564e5064196aecbf4a973ae50fd86c84eca2d
RobotCGB <cgbarrios.02@gmail.com>
2022-10-15 16:51:14 +0200

    Añadido editar equipos con sus respectivas funciones (añadirPjParty, eliminarPjParty...)

--- src/Equipo.java
+++ src/Equipo.java
@@ -9,4 +9,44 @@ public class Equipo {
         this.party = party;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void mostrarParty(){
+        for(int i = 0; i < party.size(); i++){
+            System.out.println(party.get(i).getNombre());
+        }
+    }
+
+    public Personaje getPjDeParty(String pj){
+        Personaje ret = null;
+        boolean ok = false;
+        int i = 0;
+        while (!ok && i < party.size()){
+            if(party.get(i).getNombre().equals(pj)) {
+                ret = party.get(i);
+                ok = true;
+            }
+            i++;
+        }
+        return ret;
+    }
+
+    public void añadirPjParty(Personaje pjAñadir){
+        party.add(pjAñadir);
+    }
+
+    public void eliminarPjParty(Personaje pjElim){
+        boolean ok = false;
+        int i = 0;
+        while(!ok && i < party.size()){
+            if(party.get(i).equals(pjElim)){
+                party.remove(i);
+            }
+            i++;
+        }
+
+    }
+
 }
--- src/Personaje.java
+++ src/Personaje.java
@@ -3,12 +3,14 @@ public class Personaje {
     private int HAbase;
     private int HDbase;
     private int turnoBase;
+    private int danoBase;
 
-    public Personaje(String nombre, int HAbase, int HDbase, int turnoBase){
+    public Personaje(String nombre, int HAbase, int HDbase, int turnoBase, int danoBase){
         this.nombre = nombre;
         this.HAbase = HAbase;
         this.HDbase = HDbase;
         this.turnoBase = turnoBase;
+        this.danoBase = danoBase;
     }
 
     public String getNombre() {
